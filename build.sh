6344c9333bde1e446e68d30e0051272e05095f4a
Jean-Marc Le Peuvédic <lepeuvedic@gmail.com>
2024-01-25 03:08:47 +0100

    Made directory changes temporary in build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Build the static site
-cd web-src; hugo --gc --minify
+(cd web-src; hugo --gc --minify)
 
 # Build the anima hello world web app in React
-cd hello-world; yarn build
+(cd hello-world; yarn build)
 
 # Build Collabrium's front end
 # ...
