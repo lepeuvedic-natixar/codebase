c88cf06d953d6e652a006a88f8f212a65feb4af8
Jean-Marc Le Peuvédic <lepeuvedic@gmail.com>
2024-01-25 03:05:39 +0100

    Add build script for Render.com

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Build the static site
+cd web-src; hugo --gc --minify
+
+# Build the anima hello world web app in React
+cd hello-world; yarn build
+
+# Build Collabrium's front end
+# ...
+
