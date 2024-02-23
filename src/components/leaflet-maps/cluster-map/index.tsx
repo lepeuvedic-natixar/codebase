706e006d0185a4ff0db088304a0db7fc2f532542
Alexander Shelyugov <ashelyugov@protonmail.com>
2024-02-23 11:09:58 +0100

    Use simpler map pane

--- src/components/leaflet-maps/cluster-map/index.tsx
+++ src/components/leaflet-maps/cluster-map/index.tsx
@@ -17,10 +17,12 @@ const zoom: number = 8;
 
 const ClusteredMap: FunctionComponent = () => {
   return (
-      <MapContainer center={defaultLatLng} zoom={zoom} style={{ width: '100%', height: '100%' }}>
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="© <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors" />
-        <ClusterByCategoryLayer />
-      </MapContainer>
+    <MapContainer center={defaultLatLng} zoom={zoom} style={{ width: '100%', height: '100%' }}>
+      <TileLayer url="https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png" attribution="©OpenStreetMap, ©CartoDB" />
+      <TileLayer url="https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png" attribution="©OpenStreetMap, ©CartoDB" />
+      {/* <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="© <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors" /> */}
+      <ClusterByCategoryLayer />
+    </MapContainer>
   )
 }
 
