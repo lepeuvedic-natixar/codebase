8d329abd86148502b171691bf622716444238e3a
Alexander Shelyugov <ashelyugov@protonmail.com>
2024-02-23 11:18:03 +0100

    Fix z-index bug

--- src/sections/maps-leaflet/clusters-map/index.tsx
+++ src/sections/maps-leaflet/clusters-map/index.tsx
@@ -43,7 +43,7 @@ const ClusteredMapSection: FunctionComponent = () => {
           width: "100%",
           height: "100%",
           position: "absolute"
-        }} zIndex={9999}>
+        }} zIndex={1100}>
           <EmissionsByCluster
             cluster={selectedCluster}
             onClose={onTableClose}
