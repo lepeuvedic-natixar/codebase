d74162a3df9c77897cee9c270b775e17c34dbeb0
Alexander Shelyugov <ashelyugov@protonmail.com>
2024-02-28 15:37:55 +0100

    Fix ESLint for unknown mappings

--- src/data/store/features/codemappings/UnknownCodeMappingsClient.ts
+++ src/data/store/features/codemappings/UnknownCodeMappingsClient.ts
@@ -1,41 +1,46 @@
-import { createApi } from '@reduxjs/toolkit/query/react'
-import { CodeMapping, IncompleteCodeMappingStorage } from './Types'
-import { backendBaseQueryFn } from 'data/store/config/BackendConfigs'
+import { createApi } from "@reduxjs/toolkit/query/react"
+import { backendBaseQueryFn } from "data/store/config/BackendConfigs"
+import { CodeMapping, IncompleteCodeMappingStorage } from "./Types"
 
 interface AllMappingResponse {
-    ids: string[],
-    mappings: CodeMapping[]
+  ids: string[]
+  mappings: CodeMapping[]
 }
 
 export const unknownMappingsApi = createApi({
-    reducerPath: 'unknownMappingsApi',
-    baseQuery: backendBaseQueryFn(),
-    endpoints: (builder) => ({
-        getCurrentUnknownMappings: builder.query<IncompleteCodeMappingStorage, void>({
-            query: () => ({
-                url: `/unknown_mappings`,
-                method: 'GET',
-            }),
-            transformResponse(responseMappings: AllMappingResponse): IncompleteCodeMappingStorage {
-                return {
-                    mappings: responseMappings.mappings,
-                    currentIds: responseMappings.ids,
-                    recentKnownIds: responseMappings.ids
-                }
-            }
-        }),
-        getCurrentUnknownMappingIds: builder.query<string[], void>({
-            query: () => ({
-                url: `/unknown_mapping_ids`,
-                method: 'GET',
-            }),
-        }),
+  reducerPath: "unknownMappingsApi",
+  baseQuery: backendBaseQueryFn(),
+  endpoints: (builder) => ({
+    getCurrentUnknownMappings: builder.query<
+      IncompleteCodeMappingStorage,
+      void
+    >({
+      query: () => ({
+        url: `/unknown_mappings`,
+        method: "GET",
+      }),
+      transformResponse(
+        responseMappings: AllMappingResponse,
+      ): IncompleteCodeMappingStorage {
+        return {
+          mappings: responseMappings.mappings,
+          currentIds: responseMappings.ids,
+          recentKnownIds: responseMappings.ids,
+        }
+      },
     }),
+    getCurrentUnknownMappingIds: builder.query<string[], void>({
+      query: () => ({
+        url: `/unknown_mapping_ids`,
+        method: "GET",
+      }),
+    }),
+  }),
 })
 
 export const {
-    useGetCurrentUnknownMappingsQuery,
-    useLazyGetCurrentUnknownMappingsQuery,
-    useGetCurrentUnknownMappingIdsQuery,
-    useLazyGetCurrentUnknownMappingIdsQuery
+  useGetCurrentUnknownMappingsQuery,
+  useLazyGetCurrentUnknownMappingsQuery,
+  useGetCurrentUnknownMappingIdsQuery,
+  useLazyGetCurrentUnknownMappingIdsQuery,
 } = unknownMappingsApi
