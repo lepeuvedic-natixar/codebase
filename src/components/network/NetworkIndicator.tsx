218d84aa29a541aed1ea3f719a992becdbe7097f
Alexander Shelyugov <ashelyugov@protonmail.com>
2024-03-03 16:48:23 +0100

    Add popup to the network indicator

--- src/components/network/NetworkIndicator.tsx
+++ src/components/network/NetworkIndicator.tsx
@@ -1,8 +1,8 @@
-import { Box, SxProps } from "@mui/material"
+import { Box, Popover, SxProps, Typography } from "@mui/material"
 import SensorsIcon from "@mui/icons-material/Sensors"
 import SensorsOffIcon from "@mui/icons-material/SensorsOff"
 import { useGetNetworkInformationQuery } from "data/store/features/networkIndication/NetworkCheckClient"
-import { memo } from "react"
+import { memo, useCallback, useState } from "react"
 
 const NetworkIndicator = (props: SxProps) => {
   const { ...sxProps } = props
@@ -10,15 +10,55 @@ const NetworkIndicator = (props: SxProps) => {
     pollingInterval: 2000,
   })
 
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null)
+
+  const handlePopoverOpen = useCallback(
+    (event: React.MouseEvent<HTMLElement>) => {
+      setAnchorEl(event.currentTarget)
+    },
+    [setAnchorEl],
+  )
+
+  const handlePopoverClose = useCallback(() => {
+    setAnchorEl(null)
+  }, [setAnchorEl])
+
   const networkIsOk = data && !error
+  const openPopover = Boolean(anchorEl)
+  const popoverText = networkIsOk
+    ? "Connected to server"
+    : "Lost connection to the server. Contact admins for help."
 
   return (
     <Box sx={{ ...sxProps }}>
-      {networkIsOk ? (
-        <SensorsIcon color="success" />
-      ) : (
-        <SensorsOffIcon color="warning" />
-      )}
+      <Box onMouseEnter={handlePopoverOpen} onMouseLeave={handlePopoverClose}>
+        {networkIsOk ? (
+          <SensorsIcon color="success" />
+        ) : (
+          <SensorsOffIcon color="warning" />
+        )}
+      </Box>
+      <Popover
+        sx={{
+          pointerEvents: "none",
+        }}
+        open={openPopover}
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+        onClose={handlePopoverClose}
+        disableRestoreFocus
+      >
+        <Typography sx={{ p: 1 }} variant="h6">
+          {popoverText}
+        </Typography>
+      </Popover>
     </Box>
   )
 }
