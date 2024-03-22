fa9ff243da7b1ba626c7e736045a9d4ac5f0b932
Tony DUONG <astowny@gmail.com>
2024-03-23 00:14:55 +0100

    add font weights urbanist

--- src/index.tsx
+++ src/index.tsx
@@ -35,6 +35,11 @@ import "@fontsource/public-sans/700.css"
 
 import "@fontsource/questrial/400.css"
 import "@fontsource/urbanist/400.css"
+import "@fontsource/urbanist/500.css"
+import "@fontsource/urbanist/600.css"
+import "@fontsource/urbanist/700.css"
+import "@fontsource/urbanist/800.css"
+import "@fontsource/urbanist/900.css"
 
 // project import
 import { ConfigProvider } from "contexts/ConfigContext"
