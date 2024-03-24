69cf7b256b737ae311443a9d3df253e58f457a80
Tony DUONG <astowny@gmail.com>
2024-03-24 15:11:25 +0100

    remove auth footer

--- src/sections/auth/AuthWrapper.tsx
+++ src/sections/auth/AuthWrapper.tsx
@@ -5,7 +5,6 @@ import { Typography, Stack, Box, Grid, useMediaQuery } from "@mui/material"
 import { useTheme } from "@mui/material/styles"
 
 // project import
-import AuthFooter from "components/cards/AuthFooter"
 import Logo from "components/logo"
 import AuthCard from "./AuthCard"
 import { getImageUrl, ImagePath } from "utils/getImageUrl"
@@ -46,80 +45,79 @@ const AuthWrapper = ({ children }: Props) => {
 
   const z1 = () => ({ zIndex: 1, position: 'relative' })
 
-  return (<Box position='relative' sx={bgStyle}>
-    <Box sx={filterStyle} />
-    <Grid
-      container
-      direction="column"
-      justifyContent="space-between"
-      sx={{ minHeight: "100vh", ...z1 }}
-    >
-      <Grid item xs={12} sx={{ ml: 3, mt: 3 }}>
-        <Logo />
-      </Grid>
-      <Stack direction={aboveSM ? "row" : "column"}>
-        <Grid
-          item
-          xs={12}
-          container
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Grid item>
-            <Typography variant="h1" component="h1"
-              color={"#fff"}
-              fontWeight='medium'
-              textAlign="left"
-              zIndex={1}
-              fontFamily='Urbanist'
-              position='relative'
-              fontSize={aboveSM ? 90 : 50}
-            >
-              Powering<br />
-              the <Box component='span' color="success.main" fontWeight={'bold'}>future</Box><br />
-              <Box
-                component='span'
-                sx={{
-                  width: 'fit-content',
-                  position: 'relative',
-                  zIndex: 2,
-                  "&::before": { // today text
-                    content: '"today."',
+  return (
+    <Box position='relative' sx={bgStyle}>
+      <Box sx={filterStyle} />
+      <Grid
+        container
+        direction="column"
+        justifyContent={'space-between'}
+        sx={{ minHeight: "100vh", ...z1 }}
+      >
+        <Grid item xs={12} sx={{ ml: 3, mt: 3 }}>
+          <Logo />
+        </Grid>
+        <Stack direction={aboveSM ? "row" : "column"}>
+          <Grid
+            item
+            xs={12}
+            container
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item>
+              <Typography variant="h1" component="h1"
+                color={"#fff"}
+                fontWeight='medium'
+                textAlign="left"
+                zIndex={1}
+                fontFamily='Urbanist'
+                position='relative'
+                fontSize={aboveSM ? 90 : 50}
+              >
+                Powering<br />
+                the <Box component='span' color="success.main" fontWeight={'bold'}>future</Box><br />
+                <Box
+                  component='span'
+                  sx={{
+                    width: 'fit-content',
                     position: 'relative',
                     zIndex: 2,
-                  },
-                  "&::after": { // green underline
-                    content: '""',
-                    zIndex: 1,
-                    position: 'absolute',
-                    bottom: '8px',
-                    left: 0,
-                    width: '100%',
-                    height: '0px',
-                    border: '2px solid #1DB648',
-                    borderRadius: '4px',
-                  }
-                }}></Box>
-            </Typography>
+                    "&::before": { // today text
+                      content: '"today."',
+                      position: 'relative',
+                      zIndex: 2,
+                    },
+                    "&::after": { // green underline
+                      content: '""',
+                      zIndex: 1,
+                      position: 'absolute',
+                      bottom: '8px',
+                      left: 0,
+                      width: '100%',
+                      height: '0px',
+                      border: '2px solid #1DB648',
+                      borderRadius: '4px',
+                    }
+                  }}></Box>
+              </Typography>
+            </Grid>
           </Grid>
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          container
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Grid item>
-            <AuthCard>{children}</AuthCard>
+          <Grid
+            item
+            xs={12}
+            container
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item>
+              <AuthCard>{children}</AuthCard>
+            </Grid>
           </Grid>
-        </Grid>
-      </Stack>
-      <Grid item xs={12} sx={{ m: 3, mt: 1 }}>
-        <AuthFooter />
+        </Stack>
+        <Box />
       </Grid>
-    </Grid >
-  </Box >
+    </Box>
   )
 }
 
