ff340dcc6e2f76dfdeafb78512ea254d69db3a14
Jean-Marc Le Peuvédic <lepeuvedic@gmail.com>
2024-09-25 10:18:55 +0200

    Connected Landing Page to Trace and Track App

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,22 @@
 import { createRoot } from "react-dom/client"
-import { RouterProvider } from "react-router-dom"
-import { createBrowserRouter } from "react-router-dom"
-import { Link /*, useNavigate*/ } from 'react-router-dom';
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Link /* , useNavigate */,
+} from "react-router-dom"
 
 import { styled, Theme, CSSObject } from "@mui/material/styles"
-import { Typography, Box, Stack, Card, CardContent } from '@mui/material';
+import { Typography, Box, Stack, Card, CardContent } from "@mui/material"
 
-import { useWindowSize } from 'react-window-size-hooks';
-import { useFusionAuth } from "@fusionauth/react-sdk";
+import { useWindowSize } from "react-window-size-hooks"
 import { getImageUrl, ImagePath } from "utils/getImageUrl"
-import Header from 'components/landing-page/Header';
+import Header from "components/landing-page/Header"
 
 import "@fontsource/questrial/400.css"
 
 // project import
-import { ConfigProvider } from "contexts/ConfigContext";
-//import App from "./App";
+import { ConfigProvider } from "contexts/ConfigContext"
+// import App from "./App";
 import ThemeCustomization from "themes"
 import Locales from "components/Locales"
 import RTLLayout from "components/RTLLayout"
@@ -24,106 +25,109 @@ import { store } from "data/store"
 import { Provider } from "react-redux"
 
 // auth-provider
-import { FusionAuthProvider } from "@fusionauth/react-sdk";
-import { fusionauth_config } from "./config";
+import { useFusionAuth, FusionAuthProvider } from "@fusionauth/react-sdk"
+import { fusionauth_config } from "./config"
 
 // Web vitals
-import reportWebVitals from "./reportWebVitals";
-
+import reportWebVitals from "./reportWebVitals"
 
 const LandingPage: React.FC = () => {
-  const { height, width } = useWindowSize();
-  const heightTreshold = 930
-  const widthTreshold = 1400
-  const bgImgUrl = getImageUrl(`trees-and-sky.jpg`, ImagePath.LANDING);
-  const pictoEnvironment = getImageUrl(`picto-environment.png`, ImagePath.LANDING);
-  const pictoSocial = getImageUrl(`picto-social.png`, ImagePath.LANDING);
-  const pictoGovernance = getImageUrl(`picto-governance.png`, ImagePath.LANDING);
+  const { height, width } = useWindowSize()
+  const heightThreshold = 930
+  const widthThreshold = 1400
+  const bgImgUrl = getImageUrl(`trees-and-sky.jpg`, ImagePath.LANDING)
+  const pictoEnvironment = getImageUrl(
+    `picto-environment.png`,
+    ImagePath.LANDING,
+  )
+  const pictoSocial = getImageUrl(`picto-social.png`, ImagePath.LANDING)
+  const pictoGovernance = getImageUrl(`picto-governance.png`, ImagePath.LANDING)
 
   const backgroundImageStyle = () => ({
     backgroundImage: `url("${bgImgUrl}")`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center center',
-    backgroundRepeat: 'no-repeat',
-    position: 'relative',
-    width: '100%',
-    maxHeight: height >= heightTreshold && width >= widthTreshold ? '100vh' : '100%',
-    overflowY: height >= heightTreshold && width >= widthTreshold ? 'hidden' : 'initial',
-  });
+    backgroundSize: "cover",
+    backgroundPosition: "center center",
+    backgroundRepeat: "no-repeat",
+    position: "relative",
+    width: "100%",
+    maxHeight:
+      height >= heightThreshold && width >= widthThreshold ? "100vh" : "100%",
+    overflowY:
+      height >= heightThreshold && width >= widthThreshold ? "hidden" : "initial",
+  })
 
   const filterStyle = () => ({
     content: '""',
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     left: 0,
     right: 0,
     bottom: 0,
     zIndex: 0,
-    backgroundImage: `linear-gradient(320.09deg, rgba(7, 40, 91, 0.28) 25.39%, rgba(3, 34, 71, 0.75) 57.78%, rgba(3, 34, 71, 0.4) 71.71%)`
+    backgroundImage: `linear-gradient(320.09deg, rgba(7, 40, 91, 0.28) 25.39%, rgba(3, 34, 71, 0.75) 57.78%, rgba(3, 34, 71, 0.4) 71.71%)`,
   })
 
   const mainContentStyle = () => ({
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    minHeight: '100vh',
-    position: 'relative',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: "100vh",
+    position: "relative",
     paddingTop: 18,
     paddingBottom: 4,
-    zIndex: 1
+    zIndex: 1,
   })
 
   const h2Style = (theme: Theme): CSSObject => ({
     color: "#1DB447",
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 44,
   })
 
   const textStyle = (theme: Theme): CSSObject => ({
-    display: 'flex',
-    flexDirection: 'column',
-    width: 'fit-content',
-    margin: 'auto',
-    color: 'primary.light',
-    fontWeight: 'normal',
-    position: 'relative',
-    cursor: 'pointer',
+    display: "flex",
+    flexDirection: "column",
+    width: "fit-content",
+    margin: "auto",
+    color: "primary.light",
+    fontWeight: "normal",
+    position: "relative",
+    cursor: "pointer",
     fontSize: 20,
-    "&::after": { // for animation on hover
+    "&::after": {
+      // for animation on hover
       content: '""',
-      position: 'absolute',
-      bottom: '-4px',
-      width: '100%',
-      height: '0px',
-      border: '2px solid #1DB648',
-      borderRadius: '4px',
-      transform: 'scaleX(0)',
-      transition: 'all .3s',
+      position: "absolute",
+      bottom: "-4px",
+      width: "100%",
+      height: "0px",
+      border: "2px solid #1DB648",
+      borderRadius: "4px",
+      transform: "scaleX(0)",
+      transition: "all .3s",
     },
-    '&:hover::after': {
-      transform: 'scaleX(1)'
+    "&:hover::after": {
+      transform: "scaleX(1)",
     },
   })
 
   const StyledCard = styled(Card)(({ theme }) => ({
-    backgroundColor: '#EEF7FF',
-    borderRadius: '64px',
-    width: '380px',
-    textAlign: 'center',
-    paddingTop: '20px',
-    paddingBottom: '30px',
+    backgroundColor: "#EEF7FF",
+    borderRadius: "64px",
+    width: "380px",
+    textAlign: "center",
+    paddingTop: "20px",
+    paddingBottom: "30px",
   }))
 
   const stackTextStyle = {
     alignItems: "center",
     direction: "column",
     spacing: 5,
-    mt: 6
+    mt: 6,
   }
 
-  //const navigate = useNavigate();
-
-  const {isAuthenticated, isLoading, login, register} = useFusionAuth();
+  const { login } = useFusionAuth()
 
   return (
     <Box className="landing-page" sx={backgroundImageStyle}>
@@ -131,10 +135,23 @@ const LandingPage: React.FC = () => {
       <Header />
       <Box sx={mainContentStyle}>
         <Stack gap={18} alignItems="center">
-          <Typography variant="h1" component="h1" color={"#fff"} fontWeight='normal' textAlign="center">
-            Cutting-edge environmental and social impact<br />management software solutions
+          <Typography
+            variant="h1"
+            component="h1"
+            color="#fff"
+            fontWeight="normal"
+            textAlign="center"
+          >
+            Cutting-edge environmental and social impact
+            <br />
+            management software solutions
           </Typography>
-          <Stack direction="row" flexWrap='wrap' gap={12} justifyContent="center">
+          <Stack
+            direction="row"
+            flexWrap="wrap"
+            gap={12}
+            justifyContent="center"
+          >
             {/* Card Environment */}
             <StyledCard>
               <CardContent>
@@ -142,35 +159,38 @@ const LandingPage: React.FC = () => {
                 <Typography variant="h2" component="h2" sx={h2Style}>
                   Environment
                 </Typography>
-                <Stack alignItems={stackTextStyle.alignItems} mt={stackTextStyle.mt} direction={stackTextStyle.direction} spacing={stackTextStyle.spacing}>
-                  <Link onClick={() => login()} style={{ textDecoration: 'none', color: 'inherit' }}>
-                    <Typography
-                      component="h3"
-                      sx={textStyle}
-                      variant="h3"
-                    >Climate change
+                <Stack
+                  alignItems={stackTextStyle.alignItems}
+                  mt={stackTextStyle.mt}
+                  direction={stackTextStyle.direction}
+                  spacing={stackTextStyle.spacing}
+                >
+                  <Link
+                    onClick={() => login()}
+                    style={{ textDecoration: "none", color: "inherit" }}
+                  >
+                    <Typography component="h3" sx={textStyle} variant="h3">
+                      Climate Change
                     </Typography>
                   </Link>
 
-                  <Typography
-                    component="h3"
-                    sx={textStyle}
-                    variant="h3"
-                  >Product Footprint
+                  <Typography component="h3" sx={textStyle} variant="h3">
+                    Life-cycle Analysis
                   </Typography>
 
-                  <Typography
-                    component="h3"
-                    sx={textStyle}
-                    variant="h3"
-                  >Life-cycle Analysis
-                  </Typography>
+                  <a
+                    href="https://blockchain.natixar.pro"
+                    target="_blank" // Opens the link in a new tab
+                    rel="noopener noreferrer" // Recommended for security reasons when using target="_blank"
+                    style={{ textDecoration: "none", color: "inherit" }}
+                  >
+                    <Typography component="h3" sx={textStyle} variant="h3">
+                      Trace and Track
+                    </Typography>
+                  </a>
 
-                  <Typography
-                    component="h3"
-                    sx={textStyle}
-                    variant="h3"
-                  >Green Finance
+                  <Typography component="h3" sx={textStyle} variant="h3">
+                    Green Projects
                   </Typography>
                 </Stack>
               </CardContent>
@@ -182,33 +202,26 @@ const LandingPage: React.FC = () => {
                 <Typography variant="h2" component="h2" sx={h2Style}>
                   Social
                 </Typography>
-                <Stack alignItems={stackTextStyle.alignItems} mt={stackTextStyle.mt} direction={stackTextStyle.direction} spacing={stackTextStyle.spacing}>
-                  <Typography
-                    component="h3"
-                    sx={textStyle}
-                    variant="h3"
-                  >Community impact
+                <Stack
+                  alignItems={stackTextStyle.alignItems}
+                  mt={stackTextStyle.mt}
+                  direction={stackTextStyle.direction}
+                  spacing={stackTextStyle.spacing}
+                >
+                  <Typography component="h3" sx={textStyle} variant="h3">
+                    Community impact
                   </Typography>
 
-                  <Typography
-                    component="h3"
-                    sx={textStyle}
-                    variant="h3"
-                  >Labor Standards
+                  <Typography component="h3" sx={textStyle} variant="h3">
+                    Labor Standards
                   </Typography>
 
-                  <Typography
-                    component="h3"
-                    sx={textStyle}
-                    variant="h3"
-                  >Health & Safety
+                  <Typography component="h3" sx={textStyle} variant="h3">
+                    Health & Safety
                   </Typography>
 
-                  <Typography
-                    component="h3"
-                    sx={textStyle}
-                    variant="h3"
-                  >Customer Responsibility
+                  <Typography component="h3" sx={textStyle} variant="h3">
+                    Customer Responsibility
                   </Typography>
                 </Stack>
               </CardContent>
@@ -220,33 +233,26 @@ const LandingPage: React.FC = () => {
                 <Typography variant="h2" component="h2" sx={h2Style}>
                   Governance
                 </Typography>
-                <Stack alignItems={stackTextStyle.alignItems} mt={stackTextStyle.mt} direction={stackTextStyle.direction} spacing={stackTextStyle.spacing}>
-                  <Typography
-                    component="h3"
-                    sx={textStyle}
-                    variant="h3"
-                  >Tax Transparency
+                <Stack
+                  alignItems={stackTextStyle.alignItems}
+                  mt={stackTextStyle.mt}
+                  direction={stackTextStyle.direction}
+                  spacing={stackTextStyle.spacing}
+                >
+                  <Typography component="h3" sx={textStyle} variant="h3">
+                    Tax Transparency
                   </Typography>
 
-                  <Typography
-                    component="h3"
-                    sx={textStyle}
-                    variant="h3"
-                  >Anti-corruption
+                  <Typography component="h3" sx={textStyle} variant="h3">
+                    Anti-corruption
                   </Typography>
 
-                  <Typography
-                    component="h3"
-                    sx={textStyle}
-                    variant="h3"
-                  >Shareholders Rights
+                  <Typography component="h3" sx={textStyle} variant="h3">
+                    Shareholders Rights
                   </Typography>
 
-                  <Typography
-                    component="h3"
-                    sx={textStyle}
-                    variant="h3"
-                  >Risk Management
+                  <Typography component="h3" sx={textStyle} variant="h3">
+                    Risk Management
                   </Typography>
                 </Stack>
               </CardContent>
@@ -254,16 +260,16 @@ const LandingPage: React.FC = () => {
           </Stack>
         </Stack>
       </Box>
-    </Box >
-  );
-};
+    </Box>
+  )
+}
 
 const router = createBrowserRouter([
   {
     path: "/", // Define the path
     element: <LandingPage />,
-  }
-]);
+  },
+])
 
 const App = () => (
   <ThemeCustomization>
@@ -272,7 +278,7 @@ const App = () => (
         <Locales>
           <ScrollTop>
             <FusionAuthProvider {...fusionauth_config}>
-                <RouterProvider router={router} />
+              <RouterProvider router={router} />
             </FusionAuthProvider>
           </ScrollTop>
         </Locales>
@@ -281,13 +287,12 @@ const App = () => (
   </ThemeCustomization>
 )
 
-
 const container = document.getElementById("root")
 const root = createRoot(container!)
 
 root.render(
   <ConfigProvider>
-      <App />
+    <App />
   </ConfigProvider>,
 )
 
@@ -295,4 +300,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals(console.log)
-
